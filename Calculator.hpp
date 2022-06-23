12265dc1f3af62ba4cdac08c6cb9900680bcca16
bryce_nagaj <bnagaj@bgsu.edu>
2022-06-22 22:55:09 -0400

    Added Calculator class header file

---
+++ Calculator.hpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+class Calculator {
+
+public: 
+
+    std::string month;
+    std::string expenseFilename;
+    std::string reportFilename;
+    double groceries;
+    double eatingOut;
+    double gas;
+    double rent;
+    double electricity;
+    double water;
+    double car;
+
+    bool Prompt();
+    void Groceries();
+    void EatingOut();
+    void Gas();
+    void Other();
+    void Report();
+    bool Check();
+    void Calculate();
+
+};
